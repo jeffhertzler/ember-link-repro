049ebea5e1135fbd4226f96759c21cd493b3a9c3
Jeff Hertzler <jeffhertzler@gmail.com>
2022-06-17 14:06:05 -0400

    update test

--- tests/integration/hello-test.js
+++ tests/integration/hello-test.js
@@ -11,10 +11,37 @@ module("`setupLink` example", function (hooks) {
   setupLink(hooks);
 
   test("`<Link>` component works in render tests", async function (assert) {
+    await render(hbs`
+      <Link @route="some.route" as |l|>
+        <a
+          href={{l.url}}
+          class={{if l.isActive "is-active"}}
+          {{on "click" l.transitionTo}}
+        >
+          Click me
+        </a>
+      </Link>
+    `);
+
+    const link = linkFor("some.route");
+    link.onTransitionTo = assert.step("link clicked");
+
+    // console.log(link);
+    // await this.pauseTest();
+
+    await click("a");
+
+    assert.verifySteps(["link clicked"]);
+  });
+
+  test("inside component same thing", async function (assert) {
     await render(hbs`<Hello />`);
 
     const link = linkFor("hello");
     link.onTransitionTo = assert.step("link clicked");
+
+    // console.log(link);
+    // await this.pauseTest();
 
     await click("a");
 
